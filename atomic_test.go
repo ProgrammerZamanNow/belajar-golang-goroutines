a7f85746a5374a3d1dc9c492f7a449401585cf16
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-02-15 22:13:06 +0700

    add note problem about atomic group error

--- atomic_test.go
+++ atomic_test.go
@@ -13,6 +13,13 @@ func TestAtomic(t *testing.T) {
 
 	for i := 1; i <= 1000; i++ {
 		go func() {
+			/**
+			Jika terjadi error : panic: sync: WaitGroup is reused before previous Wait has returned
+			Itu artinya, goroutine belum selesai menjalankan kode group.Add(1), naun goroutine unit test
+			sudah melakukan group.Wait(), group tidak boleh di add ketika sudah di Wait(), hal ini biasanya
+			terjadi jika resource hardware kurang cepat ketika menjalankan goroutine diawal
+			Jika hal ini terjadi, silahkan pindahkan kode group.Add(1), ke baris 15 sebelum memanggil go func()
+			*/
 			group.Add(1)
 			for j := 1; j <= 100; j++ {
 				atomic.AddInt64(&x, 1)
